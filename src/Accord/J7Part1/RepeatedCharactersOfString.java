721f04348a44220be1a90133168e96ff22f62d0d
Tamilselvan S <tamilselvatechie@gmail.com>
2023-09-06 01:12:15 +0530

    FifthCommit

--- src/Accord/J7Part1/RepeatedCharactersOfString.java
+++ src/Accord/J7Part1/RepeatedCharactersOfString.java
@@ -1,2 +1,22 @@
-package Accord.J7Part1;public class RepeatedCharactersOfString {
+package Accord.J7Part1;
+
+public class RepeatedCharactersOfString {
+    public static void main(String[] args) {
+        String str="example";
+        for (int i = 0; i < str.length(); i++) {
+            int count=0;
+            for (int j = i+1; j < str.length(); j++) {
+                if (str.charAt(i)==str.charAt(j)){
+                    count++;
+                 //   str.charAt(j)='\0';str.
+//                    char x =str.charAt(j);
+//                    char temp= Character.toUpperCase(x);
+//                    str.charAt(j)= temp;
+                }
+            }
+            if (count>0){
+                System.out.println(str.charAt(i));
+            }
+        }
+    }
 }
