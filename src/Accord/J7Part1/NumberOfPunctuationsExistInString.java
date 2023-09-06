d9b2e2d25448329a03e9e856063ff2150cc08778
Tamilselvan S <tamilselvatechie@gmail.com>
2023-09-06 11:56:03 +0530

    AddingJ7Part1Assignments

---
+++ src/Accord/J7Part1/NumberOfPunctuationsExistInString.java
@@ -0,0 +1,12 @@
+package Accord.J7Part1;
+
+import java.util.Scanner;
+
+public class NumberOfPunctuationsExist {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your string: ");
+        String str = in.nextLine();
+        
+    }
+}
