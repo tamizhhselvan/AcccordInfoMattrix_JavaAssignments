3c72cb83b0e4189ed7d03a33c88f35e87b424524
Tamilselvan S <tamilselvatechie@gmail.com>
2023-09-11 17:59:55 +0530

    Slight changes in J3_Part2

--- src/Accord/J3_Part2/PrimeTill20.java
+++ src/Accord/J3_Part2/PrimeTill20.java
@@ -6,11 +6,11 @@ public class PrimeTill20 {
         System.out.print("The prime numbers upto " + n +" is ");
         for (int i = 2; i <= n; i++) {
             int count = 0;
-            for (int j = 2; j <i-1; j++) {
+            for (int j = 2; j <i; j++) {
 
                 if ((i%j)==0){
                     count++;
-                   // break;
+                   break;
                 }
 
             }
@@ -43,33 +43,6 @@ public class PrimeTill20 {
 //          System.out.println(i);
 
 /////CHAT gpt SOLUTION
-//public class PrimeTill20 {
-//    public static void main(String[] args) {
-//        for (int i = 2; i <= 20; i++) {
-//            boolean isPrime = true; // Assume the number is prime initially
-//            for (int j = 2; j < i; j++) {
-//                if (i % j == 0) {
-//                    isPrime = false; // If divisible, not a prime
-//                    break; // No need to continue checking
-//                }
-//            }
-//            if (isPrime) {
-//                System.out.println(i); // Print the prime number
-//            }
-//        }
-//    }
-//}
-
-
-
-
-
-
-
-
-//
-//package Accord.J3Part2;
-//
 //public class PrimeTill20 {
 //    public static void main(String[] args) {
 //        for (int i = 2; i <= 20; i++) {
